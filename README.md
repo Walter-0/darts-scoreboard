2cf87ae83e53654c18fe5f2c18eb9ee213b92b96
Walter Augustine <walter.augustine10@gmail.com>
2017-07-21 13:01:41 -0400

    cleanup and fix bugs

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Darts
 A web-based ranking system for a darts league.
+
+JS
+
+Like that you used ES6, but because you didn’t include babel as a preprocessor it isn’t particularly friendly with a wide variety of browsers
+
+Also there is no handling in your code for if the result is “NaN” so your code breaks there and breaks sorting entirely
+
+Line 52 You do not account for more than a two-way tie in your logic
+
+Line 53 Instead of creating elements and appending them you just inserted the HTML
+
+Line 65 clearRankings does not actually clear the stored rankings – it clears the scores DOM but not from session memory
+
+Pressing ‘Enter’ on keyboard does not submit the form
--- js/main.js
+++ js/main.js
@@ -1,74 +1,74 @@
-const input = document.querySelector('input');
-const addButton = document.querySelector('#add');
-const clearButton = document.querySelector('#clear');
-const rankingsList = document.querySelector('#rankings-list');
-const players = [];
-
-function checkIfPlayerExists(name) {
-  // loop over all players, return index of matching name
-  for (let i = 0; i < players.length; i++) {
-    if(players[i].name === name) {
-      return i;
-    }
-  }
-  return false;
+const input = document.getElementById('enter-score')
+const addButton = document.getElementById('add')
+const clearButton = document.getElementById('clear')
+const rankingsList = document.getElementById('rankings-list')
+const players = []
+const Player = function () {
+  this.name = name
+  this.score = 0
 }
 
-function addToRankings() {
+function checkIfPlayerExists (name) {
+  // loop over all players, return index of matching name
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].name === name) {
+      return i
+    }
+  }
+  return false
+}
+
+function addToRankings () {
   // check for accidental click when empty
   if (input.value === '') {
-    return false;
+    return false
   }
 
-  let name = input.value.split(',')[0];
-  let score = parseInt(input.value.split(',')[1]);
-  let playerIndex = checkIfPlayerExists(name);
+  let name = input.value.split(',')[0]
+  let score = parseInt(input.value.split(',')[1], 10)
+  let playerIndex = checkIfPlayerExists(name)
 
   // check if existing player returned an index number
   if (typeof playerIndex === 'number') {
-    players[playerIndex].score += score;
+    players[playerIndex].score += score
   } else {
     // push new player into players array
-    players.push({'name': name, 'score': score});
+    players.push({'name': name, 'score': score})
   }
 
   // sort players array
   players.sort((a, b) => {
-    return a.score < b.score ? 1 : -1;
-  });
-
-  // update DOM with new sorting
-  const ranking = document.createElement('li');
+    return a.score < b.score ? 1 : -1
+  })
 
   // map player names and scores to rankings list as list items
   rankingsList.innerHTML = players.map((player, index) => {
-
     // use 'pt' if score is 1, 'pts' otherwise
-    let points = 'pts';
+    let points = 'pts'
     if (player.score === 1) {
-      points = 'pt';
+      points = 'pt'
     }
     // check for ties. excluding the first player, if the previous player has the same score as the current player, give the current player the same ranking
     if (index >= 1 && players[index - 1].score === players[index].score) {
-      return `<li>${index}. ${player.name}, ${player.score} ${points}</li>`;
+      return `<li>${index}. ${player.name}, ${player.score} ${points}</li>`
     } else {
       // start with index 1 instead of 0 for ranking
-      return `<li>${index + 1}. ${player.name}, ${player.score} ${points}</li>`;
+      return `<li>${index + 1}. ${player.name}, ${player.score} ${points}</li>`
     }
   })
-  .join('');
+    .join('')
 
   // clear the input field
-  input.value = "";
+  input.value = ''
 }
 
-function clearRankings() {
+function clearRankings () {
   if (confirm('Are you sure? This will delete all rankings.')) {
-    rankingsList.innerHTML = '';
+    rankingsList.innerHTML = ''
   } else {
-    return false;
+    return false
   }
 }
 
-addButton.addEventListener('click', addToRankings);
-clearButton.addEventListener('click', clearRankings);
+addButton.addEventListener('click', addToRankings)
+clearButton.addEventListener('click', clearRankings)
