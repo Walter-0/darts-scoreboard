0ba19fb44a68b6cce1e3c76e2048ea77507c2d5b
Walter Augustine <walter.augustine10@gmail.com>
2017-07-22 19:59:34 -0400

    fix logic. todo: fix multiple ties

--- js/main.js
+++ js/main.js
@@ -2,73 +2,86 @@ const input = document.getElementById('enter-score')
 const addButton = document.getElementById('add')
 const clearButton = document.getElementById('clear')
 const rankingsList = document.getElementById('rankings-list')
-const players = []
-const Player = function () {
+
+let players = []
+
+const Player = function (name, score) {
   this.name = name
-  this.score = 0
+  this.score = score
 }
 
-function checkIfPlayerExists (name) {
-  // loop over all players, return index of matching name
-  for (let i = 0; i < players.length; i++) {
-    if (players[i].name === name) {
-      return i
-    }
-  }
-  return false
-}
-
-function addToRankings () {
-  // check for accidental click when empty
-  if (input.value === '') {
-    return false
-  }
-
+function addPlayer () {
   let name = input.value.split(',')[0]
   let score = parseInt(input.value.split(',')[1], 10)
-  let playerIndex = checkIfPlayerExists(name)
 
-  // check if existing player returned an index number
-  if (typeof playerIndex === 'number') {
-    players[playerIndex].score += score
-  } else {
-    // push new player into players array
-    players.push({'name': name, 'score': score})
+  if (typeof score !== 'number' || isNaN(score)) {
+    return 'Not a number'
+  } else if (input.value === '') {
+    return false
   }
 
-  // sort players array
-  players.sort((a, b) => {
-    return a.score < b.score ? 1 : -1
-  })
+  const newPlayer = new Player(name, score)
 
-  // map player names and scores to rankings list as list items
-  rankingsList.innerHTML = players.map((player, index) => {
-    // use 'pt' if score is 1, 'pts' otherwise
-    let points = 'pts'
-    if (player.score === 1) {
-      points = 'pt'
+  if (players.length > 0) {
+    for (let i = 0; i < players.length; i++) {
+      if (players[i].name === name) {
+        players[i].score = score
+        return renderHTML()
+      }
     }
-    // check for ties. excluding the first player, if the previous player has the same score as the current player, give the current player the same ranking
-    if (index >= 1 && players[index - 1].score === players[index].score) {
-      return `<li>${index}. ${player.name}, ${player.score} ${points}</li>`
-    } else {
-      // start with index 1 instead of 0 for ranking
-      return `<li>${index + 1}. ${player.name}, ${player.score} ${points}</li>`
-    }
-  })
-    .join('')
-
-  // clear the input field
-  input.value = ''
+    players.push(newPlayer)
+    return renderHTML()
+  } else {
+    players.push(newPlayer)
+    return renderHTML()
+  }
 }
 
-function clearRankings () {
+function renderHTML () {
+  rankingsList.innerHTML = ''
+  let numTies = 0
+
+  return players.sort((a, b) => {
+    return a.score < b.score ? 1 : -1
+  })
+    .map((player, index) => {
+      let points = player.score === 1 ? 'pt' : 'pts'
+      index++
+
+      if (index >= 2) {
+        if (players[index - 2].score === player.score) {
+          console.log("there's a tie between " + players[index - 2].name + ' and ' + player.name)
+          numTies++
+          index -= numTies
+        }
+      }
+
+      let el = document.createElement('li')
+      let textNode = document.createTextNode(`${index}. ${player.name}, ${player.score} ${points}`)
+      el.appendChild(textNode)
+      rankingsList.appendChild(el)
+    })
+}
+
+function clearPlayers () {
   if (confirm('Are you sure? This will delete all rankings.')) {
-    rankingsList.innerHTML = ''
+    players = []
+    renderHTML()
   } else {
     return false
   }
 }
 
-addButton.addEventListener('click', addToRankings)
-clearButton.addEventListener('click', clearRankings)
+clearButton.addEventListener('click', clearPlayers)
+
+addButton.addEventListener('click', function () {
+  addPlayer()
+  input.value = ''
+})
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    addPlayer()
+    input.value = ''
+  }
+})
